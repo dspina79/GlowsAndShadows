c8c2f5a11adf42e3c8c63684b92c9c544210e297
Dave Spina <dspina79@gmail.com>
2021-03-02 20:31:33 -0500

    Project initialization

---
+++ GlowsAndShadows/GlowsAndShadows/GlowsAndShadowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlowsAndShadowsApp.swift
+//  GlowsAndShadows
+//
+//  Created by Dave Spina on 3/2/21.
+//
+
+import SwiftUI
+
+@main
+struct GlowsAndShadowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
