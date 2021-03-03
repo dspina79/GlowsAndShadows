b3a89149d8e59f4cb9503bfb50a59d440a2092cc
Dave Spina <dspina79@gmail.com>
2021-03-02 21:05:52 -0500

    Rainbow glow effect

--- GlowsAndShadows/GlowsAndShadows/ContentView.swift
+++ GlowsAndShadows/GlowsAndShadows/ContentView.swift
@@ -17,14 +17,34 @@ extension View {
     }
 }
 
+extension View {
+    func rainbowGlow() -> some View {
+        ForEach(0..<2) { i in
+            Rectangle()
+                .fill(AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/))
+                .frame(width: 300, height: 300)
+                .mask(self.blur(radius: 20))
+                .overlay(self.blur(radius: 5 - CGFloat(i * 5)))
+        }
+    }
+}
+
 
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Circle()
-                .fill(Color.white)
-                .frame(width: 200, height:200)
-                .glow(color: .blue, radius: 23)
+            
+            Text("Elijah")
+                .font(.system(size: 96, weight: .black, design: .rounded))
+                .foregroundColor(.white)
+                .multilineTextAlignment(.center)
+                .frame(width: 300, height: 200)
+                .rainbowGlow()
+            
+//            Circle()
+//                .fill(Color.white)
+//                .frame(width: 200, height:200)
+//                .glow(color: .blue, radius: 23)
             
                 // multiple shaodows increases depth and strength of the overall shadow
         }
