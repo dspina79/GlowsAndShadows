fe25653cfc28d004a3d5cf5344d8f91155582278
Dave Spina <dspina79@gmail.com>
2021-03-02 20:49:00 -0500

    Simple soft glow effect

--- GlowsAndShadows/GlowsAndShadows/ContentView.swift
+++ GlowsAndShadows/GlowsAndShadows/ContentView.swift
@@ -7,10 +7,30 @@
 
 import SwiftUI
 
+extension View {
+    func glow(color: Color = Color.red, radius: CGFloat = 20) -> some View {
+        self
+            .overlay(self.blur(radius: radius / 6))
+            .shadow(color: color, radius: radius)
+            .shadow(color: color, radius: radius)
+            .shadow(color: color, radius: radius)
+    }
+}
+
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Circle()
+                .fill(Color.white)
+                .frame(width: 200, height:200)
+                .glow(color: .blue, radius: 23)
+            
+                // multiple shaodows increases depth and strength of the overall shadow
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
